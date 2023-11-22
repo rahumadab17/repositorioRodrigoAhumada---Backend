4225c433d529bd975d380c33904417a81ca2a45f
rahumadab17 <rahumadab17@gmail.com>
2023-11-22 18:17:49 -0300

    Desafio 4 corrección de push

--- Entregables Backend - Coderhouse/Desafio 4/src/server.js
+++ Entregables Backend - Coderhouse/Desafio 4/src/server.js
@@ -2,23 +2,12 @@ import express from 'express';
 import { Server } from 'socket.io';
 import { engine } from 'express-handlebars';
 import { ProductManager } from './productManager.js';
-<<<<<<< Updated upstream
-import { CartManager } from './cartManager.js';
-import { productsRouter } from './Routers/productsRouter.js';
-import { cartsRouter } from './Routers/cartsRouter.js';
-import { homeRouter } from './Routers/homeRouter.js';
-import { realTimeProductsRouter } from './Routers/realTimeProductsRouter.js';
-=======
 import { webRouter } from './Routers/webRouter.js';
 
 export const productManager = new ProductManager;
->>>>>>> Stashed changes
 
 const port = 8080;
 const app = express();
-
-export const productManager = new ProductManager;
-export const cartManager = new CartManager;
 
 app.engine('.handlebars', engine());
 app.set('view engine', '.handlebars');
@@ -28,39 +17,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/static', express.static('./static'));
 
-<<<<<<< Updated upstream
-app.use('/api/products', productsRouter);
-app.use('/api/carts', cartsRouter);
-app.use('/', homeRouter)
-app.use('/', realTimeProductsRouter)
-=======
->>>>>>> Stashed changes
-
 const server = app.listen(port, () => {console.log(`Servidor Express escuchando en el puerto ${port}`);});
 
 const io = new Server(server);
 
-//app.use((req, res, next) => {
-//  req['io'] = io
-//  next()
-//})
-
 app.use('/', webRouter);
 
-<<<<<<< Updated upstream
-// Socket.io setup
-io.on('connection', (socket) => {
-  console.log('Usuario conectado', socket.id);
-
-  // Emitir la lista de productos cuando alguien se conecta
-  io.emit('updateProducts', productManager.getProducts());
-
-  // Manejar eventos cuando se agrega un nuevo producto
-  socket.on('addProduct', (productData) => {
-    productManager.addProduct(productData);
-    io.emit('updateProducts', productManager.getProducts());
-    console.log('Producto agregado exitosamente');
-=======
 io.on('connection', socket => {
   console.log('Usuario conectado', socket.id);
 
@@ -78,25 +40,6 @@ io.on('connection', socket => {
     
     callback({status: respuesta}) 
     io.sockets.emit('actualizacion', {products: productManager.getProducts()})
->>>>>>> Stashed changes
   });
 
-  // Manejar eventos cuando se elimina un producto
-  socket.on('deleteProduct', (productId) => {
-    productManager.deleteProduct(productId);
-    io.emit('updateProducts', productManager.getProducts());
-    console.log('Producto eliminado exitosamente')
-  });
-
-  // Manejar eventos de desconexión
-  socket.on('disconnect', () => {
-    console.log('Usuario desconectado');
-  });
 });
-
-<<<<<<< Updated upstream
-app.listen(port, () => {
-  console.log(`Servidor Express escuchando en el puerto ${port}`);
-});
-=======
->>>>>>> Stashed changes
